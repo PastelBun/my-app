c6db83a9c4a49cd721f456bc922750659c707b21
PastelBun <148943367+PastelBun@users.noreply.github.com>
2024-09-27 22:32:54 +0300

    savepoint, got tailwind working, changed file types

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "my-app/components/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
